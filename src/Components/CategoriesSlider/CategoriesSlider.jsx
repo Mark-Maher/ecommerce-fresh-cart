08ef01c7139641764449985148c7c4abdb72be82
Mark-Maher <markmaher383@gmailgit config --global user.email markmaher383@gmailgit config --global user.email markmaher383@gmail>
2024-02-23 16:59:33 +0200

    inital commit

--- src/Components/CategoriesSlider/CategoriesSlider.jsx
+++ src/Components/CategoriesSlider/CategoriesSlider.jsx
@@ -10,7 +10,7 @@ function CategoriesSlider() {
     infinite: true,
     speed: 500,
     slidesToShow: 4,
-    slidesToScroll: 3,
+    slidesToScroll: 4,
     // autoplay: true,
     // speed: 5000,
     // autoplaySpeed: Infinity,
