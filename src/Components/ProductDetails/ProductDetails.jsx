f8bbd041cf06b425615f822a54972e05f285e6ae
Mark-Maher <markmaher383@gmailgit config --global user.email markmaher383@gmailgit config --global user.email markmaher383@gmail>
2024-02-29 14:58:11 +0200

    inital commit

--- src/Components/ProductDetails/ProductDetails.jsx
+++ src/Components/ProductDetails/ProductDetails.jsx
@@ -63,69 +63,77 @@ function ProductDetails() {
               }}
             ></i>
           </div>
-          {data?.data.data && (
-            <div className='row align-items-center g-5'>
-              <div className='col-md-4'>
-                <div className={`${styles.MainSlider}`}>
-                  {" "}
-                  {data.data.data.images.length === 1 ? (
-                    <img
-                      src={data.data.data.images[0]}
-                      alt=''
-                      className='w-100'
-                    />
-                  ) : (
-                    <Slider {...settings}>
-                      {isFetching && <Loader />}
-                      {data.data.data.images.map((image, index) => (
-                        <img src={image} alt='' className='w-100' key={index} />
-                      ))}
-                    </Slider>
-                  )}
-                </div>
-              </div>
-              <div className='col-md-8'>
-                <h3>{data.data.data.title}</h3>
-                <p>{data.data.data.description}</p>
-                <div>
-                  <h3>{data.data.data.category.name}</h3>{" "}
-                  <h4 className='h6'>
-                    Brand Name : {data.data.data.brand.name}
-                  </h4>
-                  <h4 className='h6'>Quantity : {data.data.data.quantity}</h4>
-                  <div className='d-flex justify-content-between my-2'>
+          <div className='container'>
+            {" "}
+            {data?.data.data && (
+              <div className='row align-items-center g-5'>
+                <div className='col-md-4'>
+                  <div className={`${styles.MainSlider}`}>
                     {" "}
-                    <h4 className='h6'>Price : {data.data.data.price}</h4>
-                    <h4 className='h6'>
-                      Rating
-                      <i className='fas fa-star rating-bg rating-color ms-2'></i>{" "}
-                      {data.data.data.ratingsAverage}
-                    </h4>
-                  </div>
-                  <button
-                    className='btn addtocartbtn w-100 text-white'
-                    disabled={displaybtn ? true : false}
-                    onClick={() => {
-                      addProductToCart(productID);
-                    }}
-                  >
-                    {loading ? (
-                      <Spinner
-                        animation='border'
-                        role='status'
-                        size='sm'
-                        className='me-1'
-                      >
-                        <span className='visually-hidden '>Loading...</span>
-                      </Spinner>
+                    {data.data.data.images.length === 1 ? (
+                      <img
+                        src={data.data.data.images[0]}
+                        alt=''
+                        className='w-100'
+                      />
                     ) : (
-                      " add to cart"
+                      <Slider {...settings}>
+                        {isFetching && <Loader />}
+                        {data.data.data.images.map((image, index) => (
+                          <img
+                            src={image}
+                            alt=''
+                            className='w-100'
+                            key={index}
+                          />
+                        ))}
+                      </Slider>
                     )}
-                  </button>
+                  </div>
+                </div>
+                <div className='col-md-8'>
+                  <h3>{data.data.data.title}</h3>
+                  <p>{data.data.data.description}</p>
+                  <div>
+                    <h3>{data.data.data.category.name}</h3>{" "}
+                    <h4 className='h6'>
+                      Brand Name : {data.data.data.brand.name}
+                    </h4>
+                    <h4 className='h6'>Quantity : {data.data.data.quantity}</h4>
+                    <div className='d-flex justify-content-between my-2'>
+                      {" "}
+                      <h4 className='h6'>Price : {data.data.data.price}</h4>
+                      <h4 className='h6'>
+                        Rating
+                        <i className='fas fa-star rating-bg rating-color ms-2'></i>{" "}
+                        {data.data.data.ratingsAverage}
+                      </h4>
+                    </div>
+                    <button
+                      className='btn addtocartbtn w-100 text-white'
+                      disabled={displaybtn ? true : false}
+                      onClick={() => {
+                        addProductToCart(productID);
+                      }}
+                    >
+                      {loading ? (
+                        <Spinner
+                          animation='border'
+                          role='status'
+                          size='sm'
+                          className='me-1'
+                        >
+                          <span className='visually-hidden '>Loading...</span>
+                        </Spinner>
+                      ) : (
+                        " add to cart"
+                      )}
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          )}
+            )}
+          </div>
         </div>
       </section>
     </>
